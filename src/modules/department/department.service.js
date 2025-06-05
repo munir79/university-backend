6f457a00f37515fa10b2acab13b9b6d579d6d14a
munir79 <jakirhossainmunir79@gmail.com>
2025-06-05 21:29:39 +0600

    department model and service done

---
+++ src/modules/department/department.service.js
@@ -0,0 +1,41 @@
+import { DepartmentModel } from "./departmnet.model"
+
+const createDepartmentIntoDb=async(payLoad)=>{
+    const result=await DepartmentModel.create(payLoad);
+    return result;
+
+}
+
+// get all department
+
+const getAllDepartmentFromDb=async()=>{
+    const result=await DepartmentModel.find().populate('faculty');
+    return result;
+}
+
+
+// get single deparment 
+
+const getSingleDeparmentFromDb=async(id)=>{
+    const result =await DepartmentModel.findById(id).populate('faculty');
+    return result;
+
+}
+
+// const delete deopartment
+
+const deleteDepartmentInDb=async(id)=>{
+    const result =await DepartmentModel.findByIdAndDelete(id);
+    return result;
+
+}
+
+// update department
+const updateDepartment=async(id,payLOad)=>{
+    const result=await DepartmentModel.findByIdAndUpdate(id,payLOad,{
+        new:true
+    });
+    return result
+}
+
+export const DepartmentService={createDepartmentIntoDb,getAllDepartmentFromDb,getSingleDeparmentFromDb,updateDepartment}
---
+++ src/modules/department/departmnet.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const departmentSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+    },
+  faculty:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'FacultyModel',
+    required:true,
+    
+  }
+},
+{
+    timestamps:true
+})
+
+export const DepartmentModel=mongoose.model('Department',departmentSchema);
